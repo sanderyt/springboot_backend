0a1e9b3f5226edbfb809c1f55720efa382499d80
Sander van Rijsoort <s.rijsoort@gmail.com>
2023-11-21 17:53:45 +0100

    first setup

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,19 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(new Student(1L, "Sander", 21, LocalDate.of(2000, Month.JULY, 5), "s.rijsoort@gmail.com"));
+    }
+}
