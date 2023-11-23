b5c49e5fcd466e01eee60e6e9fa385d2a61d7173
Sander van Rijsoort <s.rijsoort@gmail.com>
2023-11-23 19:05:04 +0100

    added POST new student

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,19 +1,28 @@
 package com.example.demo.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "Sander", 21, LocalDate.of(2000, Month.JULY, 5), "s.rijsoort@gmail.com"));
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void createStudent(@RequestBody Student student) {
+        studentService.createStudent(student);
     }
 }
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void createStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentByEmail.isPresent()) {
+            throw new IllegalStateException("Email is taken");
+        }
+
+        studentRepository.save(student);
+    }
+
+}
